91094f01c3d48e874512ee203d9943e28fcb7ccd
Sam Desborough <sam.desborough@gmail.com>
2018-01-24 09:33:22 +0000

    circleCI tweaks

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -3,6 +3,6 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get install -y build-essential nodejs golang yarn
+sudo apt-get install -y build-essential nodejs yarn
 
 cd static && yarn install
