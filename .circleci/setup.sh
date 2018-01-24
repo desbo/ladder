c01830c89ed5b059765a416af1982762cc0c1b9e
Sam Desborough <sam.desborough@gmail.com>
2018-01-24 09:41:06 +0000

    circle tweaks

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,8 +1,11 @@
+apt-get install sudo 
+
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+sudo apt-get update
 sudo apt-get install -y build-essential nodejs yarn
 
 cd static && yarn install
